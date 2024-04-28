9c19bb7bcd1472a6cef2e25a848cb9f418bb0cbe
Quinten Steenhuis <qsteenhuis@gmail.com>
2024-04-27 20:24:16 -0400

    Bump version; add missing files

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.HousingSearchLog1',
-      version='1.1',
+      version='2.0.0',
       description=('Housing Search Log'),
       long_description='# docassemble.HousingSearch\r\n\r\nHousing Search Log\r\n\r\n## Author\r\n\r\nAna Morrissette\r\n\r\n',
       long_description_content_type='text/markdown',
